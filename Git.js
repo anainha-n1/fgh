c12b4d6586cdba553c987e63610e7c76675b29c1
ana <ana.b.rodrigues32@aluno.senai.br>
2025-04-07 15:22:59 -0300

    incluido coordenação da lista

--- Git.js
+++ Git.js
@@ -1,3 +1,7 @@
 let comidas = ['lasanha', 'pizza', 'Sushi', 'Hamburguer']
 console.log(`lista de comidas:${comidas}`);
 console.log(`primeira comida: ${comidas[0]}`);
+console.sort();
+console.log(`Comidas Ordenadas: ${comidas}`);
+
+
